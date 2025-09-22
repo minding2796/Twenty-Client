76bb5d43f754cbb121915e1a682a9f69e7c88aaa
minding2796 <minding2796@gmail.com>
2025-09-23 06:54:10 +0900

    Initial Commit

---
+++ Assets/Scripts/StockManager.cs
@@ -0,0 +1,104 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Random = UnityEngine.Random;
+
+public class StockManager : MonoBehaviour
+{
+    [Header("주식 가격 관리에 필요한 게임오브젝트")]
+    [SerializeField]
+    private TextMeshProUGUI stockPriceText;
+    [SerializeField]
+    private UnityEngine.UI.Button nextTurnButton;
+    [SerializeField]
+    private LineRenderer priceLineRenderer;
+    
+    [Header("주식 가격에 관여하는 변수")]
+    [SerializeField]
+    private int basePrice = 20;
+    [SerializeField]
+    private int minPrice = 10;
+    [SerializeField]
+    private int maxPrice = 30;
+    private int _stockPrice = 20;
+    private readonly LinkedList<float> _priceHistory = new();
+    private int _currentHistoryIndex;
+
+    private void Start()
+    {
+        nextTurnButton.onClick.AddListener(OnNextTurn);
+        InitializePriceHistory();
+        UpdateStockPriceDisplay();
+    }
+
+    private void Update()
+    {
+        OnNextTurn();
+    }
+
+    private void InitializePriceHistory()
+    {
+        for (var i = 0; i < 49; i++)
+        {
+            OnNextTurn();
+            _priceHistory.AddFirst(_stockPrice);
+        }
+        _stockPrice = basePrice;
+        _priceHistory.AddFirst(_stockPrice);
+        UpdateLineRenderer();
+    }
+
+    private void UpdateLineRenderer()
+    {
+        var positions = new Vector3[_priceHistory.Count];
+        var idx = 0;
+        foreach (var i in _priceHistory)
+        {
+            positions[idx++] = new Vector3(idx * -0.1f, (i - minPrice) / (maxPrice - minPrice), 0);
+        }
+        priceLineRenderer.positionCount = positions.Length;
+        priceLineRenderer.SetPositions(positions);
+    }
+
+    private void UpdateStockPriceDisplay()
+    {
+        stockPriceText.text = _stockPrice.ToString();
+    }
+
+    private void OnNextTurn()
+    {
+        var difference = basePrice - _stockPrice;
+        var maxChange = Mathf.Max(2, Mathf.Abs(difference));
+        int change;
+
+        if (Random.Range(0, 100) < 1)
+        {
+            change = Random.Range(-10, 11);
+            _stockPrice += change;
+        }
+        else
+        {
+            var probability = Mathf.Max(0.2f, 1f - Mathf.Abs(difference) / (float)(maxPrice - minPrice));
+
+            if (Random.value < probability)
+            {
+                change = difference switch
+                {
+                    > 0 => Random.Range(-maxChange / 2, maxChange + 1),
+                    < 0 => Random.Range(-maxChange, maxChange / 2 + 1),
+                    _ => Random.Range(-3, 4)
+                };
+            }
+            else change = Random.Range(-1, 2);
+
+            _stockPrice += change;
+            _stockPrice = Mathf.Clamp(_stockPrice, minPrice, maxPrice);
+        }
+
+        _priceHistory.AddFirst(_stockPrice);
+        while (_priceHistory.Count > 50) _priceHistory.RemoveLast();
+        UpdateLineRenderer();
+        UpdateStockPriceDisplay();
+    }
+}
